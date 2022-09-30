4c1eb11d113aaf119f72c973a3c19dd7d41ea199
Carlos Robles <cerobles@bancolombia.com.co>
2022-09-30 09:05:02 -0500

    Se crea la logica para crear las entidades

--- src/cafe/cafe.service.ts
+++ src/cafe/cafe.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError } from '../shared/errors/business-errors';
+import { BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { CafeEntity } from './cafe.entity';
 
 @Injectable()
-export class CafeService {}
+export class CafeService {
+    constructor(
+        @InjectRepository(CafeEntity)
+        private readonly cafeRepository: Repository<CafeEntity>
+    ) { }
+    
+    async createCafe(cafe: CafeEntity): Promise<CafeEntity> {
+        if (cafe.precio < 0) {
+            throw new BusinessLogicException("El precio del cafe debe ser positivo", BusinessError.NOT_FOUND);
+        } else
+            return await this.cafeRepository.save(cafe);
+    }
+}
--- src/tienda/tienda.service.ts
+++ src/tienda/tienda.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError } from '../shared/errors/business-errors';
+import { BusinessLogicException } from '../shared/errors/business-errors';
+import { Repository } from 'typeorm';
+import { TiendaEntity } from './tienda.entity';
 
 @Injectable()
-export class TiendaService {}
+export class TiendaService {
+    constructor(
+        @InjectRepository(TiendaEntity)
+        private readonly tiendaRepository: Repository<TiendaEntity>
+    ) { }
+    
+    async createTienda(tienda: TiendaEntity): Promise<TiendaEntity> {
+        if (tienda.telefono.length !== 10) {
+            throw new BusinessLogicException("El telefono debe contener 10 caracteres", BusinessError.NOT_FOUND);
+        } else
+            return await this.tiendaRepository.save(tienda);
+    }
+}
